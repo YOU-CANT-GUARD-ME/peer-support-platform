5da083285b37af0e204c2fde4b60f38065904595
Paul Harrsion <domfam60@gmail.com>
2025-11-27 20:02:56 +0900

    testing voice chat

--- frontend/src/pages/GroupDetail.jsx
+++ frontend/src/pages/GroupDetail.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect, useRef } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("[http://localhost:3001](http://localhost:3001)"); // 서버 주소
+const socket = io("http://localhost:5000");
+; // 서버 주소
 
 export default function GroupDetailPage() {
 /* -----------------------------
